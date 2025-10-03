6bb54be024e258b7624f8f040421d4f89f66f2b2
Sivaraman S V <sv3052@srmist.edu.in>
2025-10-03 21:27:12 +0530

    Made the Landing Page fully responsive

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sudoku-Project
 This is a Practice Project where i learnt the fundamentals of web development - HTML, CSS, and JavaScript. 
+It showcases multiple skills like:
+1. Responsive Web Design
+2. CSS Flexbox model and Media Queries
+3. JavaScript Basics
+4. Built with Basic Accessibility Features
+5. Deployment on Vercel (free tier)
+
--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("Hello, World!");
 console.log(`I like Pizza!`);
+
+function showSidebar(){
+    event.preventDefault();
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+} 
+
+console.log("JavaScript is connected successfully!");
